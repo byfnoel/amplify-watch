28dc568037072e1e88a817962135ee4775126a99
Francisco Noel <63328653+byfnoel@users.noreply.github.com>
2024-03-28 19:23:43 -0400

    Establish connection to aws amplify

---
+++ examples/basic.rs
@@ -0,0 +1,8 @@
+use amplify_watch::establish_connection;
+
+#[allow(unused_variables)]
+
+fn main() {
+    let conn = establish_connection();
+    println!("Hello, world!");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
-//! AMPLIFY-WATCH
-//! A ```Rust``` monitoring library tool for AWS Amplify
+use aws_config::load_from_env;
+use aws_sdk_amplify as amplify;
+//use thiserror::Error;
 
-#[allow(dead_code)]
-fn main() {
-    /* This is a list on everything we have to do */
+#[allow(dead_code, unused_imports, clippy::expect_used)]
+#[tokio::main]
+async fn main() {
+    match establish_connection().await {
+        Ok(_) => todo!(),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+}
 
-    println!("Hello, world!");
+pub async fn establish_connection() -> Result<(), aws_sdk_amplify::Error> {
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_amplify::Client::new(&config);
+
+    // ... make some calls with the client
+    Ok(())
 }

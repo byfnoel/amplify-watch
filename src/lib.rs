ccb8a59d90b648e87b0c32149a0d4c0007324559
Francisco Noel <63328653+byfnoel@users.noreply.github.com>
2024-04-03 10:43:51 -0400

    Get logs data from aws amplify

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use aws_config::load_from_env;
 use aws_sdk_amplify as amplify;
-//use thiserror::Error;
+use aws_sdk_amplify::operation::generate_access_logs::GenerateAccessLogs;
+use chrono::{DateTime, Local};
 
-#[allow(dead_code, unused_imports, clippy::expect_used)]
 #[tokio::main]
 async fn main() {
     match establish_connection().await {
@@ -12,9 +12,29 @@ async fn main() {
 }
 
 pub async fn establish_connection() -> Result<(), aws_sdk_amplify::Error> {
-    let config = aws_config::load_from_env().await;
-    let client = aws_sdk_amplify::Client::new(&config);
+    let config = load_from_env().await;
+    let client = amplify::Client::new(&config);
 
-    // ... make some calls with the client
+    // make the call to `get_logs` here
+    Ok(())
+}
+
+pub struct LogsRequestInput {
+    pub start_time: DateTime<Local>,
+    pub end_time: DateTime<Local>,
+    pub domain_name: String,
+    pub app_id: String,
+}
+
+pub async fn get_logs(logs_request_input: LogsRequestInput) -> Result<(), GenerateAccessLogs> {
+    let config = load_from_env().await;
+    let client = amplify::Client::new(&config);
+
+    let request = client
+        .generate_access_logs()
+        .app_id("app_id")
+        .domain_name("domain_name");
+
+    let response = request.send().await;
     Ok(())
 }

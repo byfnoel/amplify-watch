473e5f97e3e4ead696d59f0f50c8ca37193f6690
Francisco Noel <63328653+byfnoel@users.noreply.github.com>
2024-09-22 21:52:34 -0400

    Fix aws connection errors

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use amplify::types::JobStatus;
+use amplify::{Client, Error};
+use aws_config::meta::region::RegionProviderChain;
+use aws_config::{from_env, BehaviorVersion};
+use aws_sdk_amplify as amplify;
+#[::tokio::main]
+async fn main() -> Result<(), Error> {
+    match establish_connection().await {
+        Ok(_) => {
+            println!("Successfully connected!")
+        }
+        Err(e) => {
+            println!("Cannot establish connection because {:?}", e);
+        }
+    }
+    Ok(())
+}
+
+async fn establish_connection() -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = from_env()
+        .behavior_version(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
+    let client = Client::new(&config);
+    let apps = client.list_apps().send().await?;
+    println!("Amplify apps: {:?}", apps);
+    Ok(())
+}
+async fn check_amplify_job_status(
+    client: &Client,
+    app_id: &str,
+    branch_name: &str,
+    job_id: &str,
+) -> Result<String, Error> {
+    let job_result = client
+        .get_job()
+        .app_id(app_id)
+        .branch_name(branch_name)
+        .job_id(job_id)
+        .send()
+        .await?;
+
+    if let Some(job) = job_result.job() {
+        if let Some(summary) = job.summary() {
+            match summary.status() {
+                Some(summary) => match summary {
+                    JobStatus::Pending => Ok("Job is pending".to_string()),
+                    JobStatus::Provisioning => Ok("Job is provisioning".to_string()),
+                    JobStatus::Running => Ok("Job is running".to_string()),
+                    JobStatus::Failed => Ok("Job has failed".to_string()),
+                    JobStatus::Succeed => Ok("Job has succeeded".to_string()),
+                    JobStatus::Cancelling => Ok("Job is being cancelled".to_string()),
+                    JobStatus::Cancelled => Ok("Job was cancelled".to_string()),
+                    _ => Ok("Unknown job status".to_string()),
+                },
+                None => Ok("Job status not available".to_string()),
+            }
+        } else {
+            Ok("Job summary not available".to_string())
+        }
+    } else {
+        Ok("Job not found".to_string())
+    }
+}

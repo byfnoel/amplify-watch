adff25cbcd317d967f05177265ac8f588efe11c8
Francisco Noel <63328653+byfnoel@users.noreply.github.com>
2025-01-27 14:05:21 -0500

    Monitor your amplify app on the terminal

--- examples/basic.rs
+++
@@ -1,6 +0,0 @@
-extern crate amplify_watch;
-
-fn main() {
-    let connection = amplify_watch::main();
-    println!("{:#?}", connection);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,46 @@
-use amplify::types::JobStatus;
-use amplify::{Client, Error};
-use aws_config::meta::region::RegionProviderChain;
-use aws_config::{from_env, BehaviorVersion};
-use aws_sdk_amplify as amplify;
+use aws_sdk_amplify::Client;
+use colored::*;
+use dotenv::dotenv;
+use std::{error::Error, time::Duration};
+use tokio::time;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    match establish_connection().await {
-        Ok(_) => {
-            println!("Successfully connected!");
-        }
-        Err(e) => {
-            println!("Cannot establish connection because {:?}", e);
-        }
-    }
-    Ok(())
-}
-
-async fn establish_connection() -> Result<(), Error> {
-    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
-    let config = from_env()
-        .behavior_version(BehaviorVersion::latest())
-        .region(region_provider)
-        .load()
-        .await;
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+    let config = aws_config::load_from_env().await;
     let client = Client::new(&config);
-    let apps = client.list_apps().send().await?;
-    println!("Amplify apps: {:?}", apps);
-    Ok(())
-}
 
-async fn check_amplify_job_status(
-    client: &Client,
-    app_id: &str,
-    branch_name: &str,
-    job_id: &str,
-) -> Result<String, Error> {
-    let job_result = client
-        .get_job()
-        .app_id(app_id)
-        .branch_name(branch_name)
-        .job_id(job_id)
-        .send()
-        .await?;
+    println!(
+        "{}",
+        "🚀 AWS Amplify Real-Time Status Monitor 🚀"
+            .bright_magenta()
+            .bold()
+    );
 
-    if let Some(job) = job_result.job() {
-        if let Some(summary) = job.summary() {
-            match summary.status() {
-                Some(status) => match status {
-                    JobStatus::Pending => Ok("Job is pending".to_string()),
-                    JobStatus::Provisioning => Ok("Job is provisioning".to_string()),
-                    JobStatus::Running => Ok("Job is running".to_string()),
-                    JobStatus::Failed => Ok("Job has failed".to_string()),
-                    JobStatus::Succeed => Ok("Job has succeeded".to_string()),
-                    JobStatus::Cancelling => Ok("Job is being cancelled".to_string()),
-                    JobStatus::Cancelled => Ok("Job was cancelled".to_string()),
-                },
-                None => Ok("Job status not available".to_string()),
+    let app_id = std::env::var("AMPLIFY_APP_ID")
+        .unwrap_or_else(|_| panic!("AMPLIFY_APP_ID must be set in the environment"));
+    let branch_name = std::env::var("AMPLIFY_BRANCH_NAME").unwrap_or_else(|_| "master".to_string());
+
+    loop {
+        match client
+            .list_jobs()
+            .app_id(&app_id)
+            .branch_name(&branch_name)
+            .send()
+            .await
+        {
+            Ok(response) => {
+                for job_summary in &response.job_summaries {
+                    println!(
+                        "   Job id  {:#?} =>  {:#?}",
+                        job_summary.job_id(),
+                        job_summary.status()
+                    );
+                }
             }
-        } else {
-            Ok("Job summary not available".to_string())
+            Err(e) => eprintln!("Error fetching jobs: {}", e),
         }
-    } else {
-        Ok("Job not found".to_string())
+
+        time::sleep(Duration::from_secs(8)).await;
     }
 }
